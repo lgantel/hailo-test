ad1f7da4678d53032f39caeef3b499c368145832
laurent.gantel <laurent.gantel@hesge.ch>
2024-05-17 17:48:23 +0200

    Add code to test libhailort dependencies

--- hailo-test.cpp
+++ hailo-test.cpp
@@ -1,5 +1,28 @@
+#include "hailo-test.hpp"
+#include "hailo/device.hpp"
+
 #include <iostream>
 
+/**
+ * @brief Main entry point 
+ */
 int main(int argc, char ** argv) {
-    printf("Hello Hailo!\n");
+    std::cout << "Hello Hailo!" << std::endl;
+
+    auto device_ids = Device::scan();
+    CHECK_EXPECTED_AS_STATUS(device_ids);
+
+    if (device_ids->size() == 0) {
+        std::cout << "Hailo devices not found" << std::endl;
+    }
+    else {
+        std::cout << "Hailo Devices:" << std::endl;
+        for (const auto& device_id : device_ids.value()) {
+            std::cout << "[-] Device: " << device_id << std::endl;
+        }
+    }
+
+    return HAILO_SUCCESS;
+
+    return 0;
 }
